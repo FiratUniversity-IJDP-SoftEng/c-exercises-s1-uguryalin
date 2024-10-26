5ec5ff061fbdff5bdfe5b282012535e4ffb8dc06
UĞUR <yalinugur00@outlook.com>
2024-10-26 16:48:58 +0300

    Update ex5.c

--- ex5.c
+++ ex5.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+void sayHello(char name[]) {
+    printf("Hello, %s!\n", name);
+}
+int main() {
+    char name[50];
+    printf("Please enter your name: ");
+    scanf("%s", name);
+    sayHello(name);
+    return 0;
+}
