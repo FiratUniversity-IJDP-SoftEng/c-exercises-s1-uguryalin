e73e37d22f1614cb95064453bc71191e27e4303e
UĞUR <yalinugur00@outlook.com>
2024-10-26 16:31:55 +0300

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main() {
+    int age;
+    printf("Please enter your age: ");
+    scanf("%d", &age);
+    for (int i = 0; i < age; i++) {
+        printf("%d. Happy Birthday!\n", i+1);
+    }
+    return 0;
+}
