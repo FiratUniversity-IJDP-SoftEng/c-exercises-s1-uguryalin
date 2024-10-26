dbbf5266ddd402089e8b799359c56462b676ffb1
UĞUR <yalinugur00@outlook.com>
2024-10-26 16:18:40 +0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,1 +1,17 @@
-printf("Hello");
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main() {
+    int guess;
+    int correctNumber;
+    srand(time(0));
+    correctNumber = rand() % 5 + 1;
+    printf("Guess a number between 1 and 5: ");
+    scanf("%d", &guess);
+    if (guess == correctNumber) {
+        printf("Perfect! You guessed it right.\n");
+    } else {
+        printf("Sorry:(, you guessed wrong. The correct number was %d.\n", correctNumber);
+    }
+    return 0;
+}
