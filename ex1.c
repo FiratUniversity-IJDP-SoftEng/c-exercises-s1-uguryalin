1516416ae3bd22c49756a34fdbfff7d42a7232a8
UĞUR <yalinugur00@outlook.com>
2024-10-26 16:23:39 +0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 int main() {
-    int guess;
-    int correctNumber;
-    srand(time(0));
-    correctNumber = rand() % 5 + 1;
+    int guess; // Kullanıcının tahmini
+    int secretNumber = 3;
     printf("Guess a number between 1 and 5: ");
     scanf("%d", &guess);
-    if (guess == correctNumber) {
-        printf("Perfect! You guessed it right.\n");
+    if (guess == secretNumber) {
+        printf("Congratulations! You guessed it right.\n");
     } else {
-        printf("Sorry:(, you guessed wrong. The correct number was %d.\n", correctNumber);
+        printf("Sorry, you guessed wrong. The correct number was %d.\n", secretNumber);
     }
     return 0;
 }
+
