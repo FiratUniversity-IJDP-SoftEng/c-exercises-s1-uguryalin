0d9df8d38ca8c3c70fe3f6902e50294c1315b209
UĞUR <yalinugur00@outlook.com>
2024-10-26 16:41:44 +0300

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char pet[10];
+    printf("Do you have a cat or a dog? ");
+    scanf("%s", pet);
+    if (strcmp(pet, "cat") == 0) {
+        printf("A cat says: Meow!\n");
+    } else if (strcmp(pet, "dog") == 0) {
+        printf("A dog says: Woof!\n");
+    } else {
+        printf("I don't know that pet:(\n");
+    }
+    return 0;
+}
